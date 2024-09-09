e031622204be58811713f7713b882d41d82ac92c
Kartic Bedi <me@karticbedi.com>
2024-09-09 20:53:10 +0530

    added main base for dashboard

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,17 @@
+<div class="hidden border-r bg-gray-50 md:block">
+    <div class="flex h-full max-h-screen flex-col gap-2">
+        <div class="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6"><a href="/"
+                class="flex items-center gap-2 font-semibold">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-package2-icon h-6 w-6">
+                    <path d="M3 9h18v10a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V9Z"></path>
+                    <path d="m3 9 2.45-4.9A2 2 0 0 1 7.24 3h9.52a2 2 0 0 1 1.8 1.1L21 9"></path>
+                    <path d="M12 3v6"></path>
+                </svg><span class="">Kashmir Online</span></a>
+        </div>
+        <div class="flex-1">
+            @include('layouts.navigation')
+        </div>
+    </div>
+</div>
