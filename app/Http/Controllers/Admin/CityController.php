8d92af765f7d5c6aa0d6f5192403e1b3bdf72582
Kartic Bedi <karticbedi@gmail.com>
2024-09-11 18:09:31 +0530

    Update: city controller and model

--- app/Http/Controllers/Admin/CityController.php
+++ app/Http/Controllers/Admin/CityController.php
@@ -5,6 +5,11 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\Log;
+use App\Models\City;
 
 class CityController extends Controller
 {
@@ -14,6 +19,44 @@ class CityController extends Controller
     public function index()
     {
         return view('city.index');
+    }
+
+    /**
+     * Create the city form.
+     */
+    public function create(Request $request): View
+    {
+        return view('city.edit');
+    }
+
+    /**
+     * Store city form.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        try {
+            $data = $request->all();
+            $request->validate([
+                            'name'      => 'required|string',
+                            'slug'      => 'required|string',
+                            'zip_code'  => 'required|string'
+                        ]);
+
+            $response = City::create([
+                                    'name'               => $data['name'],
+                                    'slug'               => $data['slug'],
+                                    'description'        => $data['description'],
+                                    'highlights_content' => $data['highlights_content'],
+                                    'zip_code'           => $data['zip_code'],
+                                    'image'              => $data['image'],
+                                    'image_alt'          => $data['image_alt'],
+                                    //'time_to_visit'      => $data['time_to_visit'],
+                                ]);
+
+            return Redirect::route('city.edit',$response->id);
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in city store.');
+        }
     }
 
     /**
--- app/Models/City.php
+++ app/Models/City.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+                            'name',
+                            'slug',
+                            'description',
+                            'highlights_content',
+                            'zip_code',
+                            'image',
+                            'image_alt',
+                            'meta_id',
+                            'time_to_visit'
+                        ];
 }
