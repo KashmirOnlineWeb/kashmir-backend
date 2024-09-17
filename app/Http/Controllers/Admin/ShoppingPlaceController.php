ae4ce48274486ed2839adf63f070150435be63d6
Chander Bedi <chanderbedi88@gmail.com>
2024-09-17 18:25:57 +0530

    Create: shopping places all methods.

--- app/Http/Controllers/Admin/ShoppingPlaceController.php
+++ app/Http/Controllers/Admin/ShoppingPlaceController.php
@@ -3,73 +3,170 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-//use App\Models\ShoppingPlace;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
+use App\Models\ShoppingPlace;
 use App\Models\City;
+use App\Models\Meta;
 
 class ShoppingPlaceController extends Controller
 {
+    /**
+     * Display list of shoppingPlaces
+     */
     public function index()
     {
-        //$shoppingPlaces = ShoppingPlace::all();
-        // add some dummy collection data
-        $shoppingPlaces = collect([
-            (object)['id' => 1, 'name' => 'Shopping Place 1', 'city' => (object)['name' => 'City 1'], 'status' => 1, 'contact' => '1234567890', 'created_at' => now(), 'updated_at' => now()],
-            (object)['id' => 2, 'name' => 'Shopping Place 2', 'city' => (object)['name' => 'City 2'], 'status' => 0, 'contact' => '1234567890', 'created_at' => now(), 'updated_at' => now()],
-            (object)['id' => 3, 'name' => 'Shopping Place 3', 'city' => (object)['name' => 'City 3'], 'status' => 1, 'contact' => '1234567890', 'created_at' => now(), 'updated_at' => now()],
-        ]);
+        try {
+            $shoppingPlaces = ShoppingPlace::orderBy('id','desc')->paginate(12);
+            return view('Shoppingplace.index')->with(['shoppingPlaces' => $shoppingPlaces]);
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in ShoppingPlace index.');
+        }
+    }
 
-        $perPage = 10; // Number of items per page
-        $currentPage = request()->get('page', 1); // Get the current page from the request, default to 1
-        $pagedData = $shoppingPlaces->forPage($currentPage, $perPage);
+    /**
+     * Create shoppingPlace form.
+     */
+    public function create(Request $request): View
+    {
+        $cities = City::select('id','name')->get();
+        return view('Shoppingplace.edit',compact('cities'));
+    }
 
-        $shoppingPlaces = new \Illuminate\Pagination\LengthAwarePaginator(
-            $pagedData,
-            $shoppingPlaces->count(),
-            $perPage,
-            $currentPage,
-            ['path' => request()->url(), 'query' => request()->query()]
-        );
+    /**
+     * Store shopping form.
+     */
+    public function store(Request $request): RedirectResponse
+    {
+        try {
+            $data = $request->all();
+            
+            $request->validate([
+                            'name'            => 'required|string',
+                            'title'           => 'required|string',
+                            'image'           => 'sometimes|string',
+                            'image_alt'       => 'sometimes|string',
+                            'repeater_content'=> 'sometimes|array',
+                            'city_id'         => 'required|integer|exists:cities,id',
+                            'meta_title'      => 'required|string|nullable',
+                            'meta_description'=> 'required|string|nullable',
+                            'keywords'        => 'required|string|nullable'
+                        ]);
+
+            /* Insert Meta */
+            if((!empty($data['meta_title'])) || (!empty($data['meta_description'])) || (!empty($data['keywords']))){
+                $meta = Meta::create([
+                            'meta_title'       => $data['meta_title'],
+                            'meta_description' => $data['meta_description'],
+                            'keywords'         => $data['keywords'],
+                            'status'           => 1,
+                        ]);
+            }
+            
+            $response = ShoppingPlace::create([
+                                        'name'            => $data['name'],
+                                        'title'           => $data['title'],
+                                        'image'           => $data['image'],
+                                        'image_alt'       => $data['image_alt'],
+                                        'repeater_content'=> $data['repeater_content'],
+                                        'city_id'         => $data['city_id'],
+                                        'meta_id'         => $meta->id,
+                                    ]);
+
+
+            return Redirect::route('Shoppingplace.index',$response->id)->with('success', 'Shopping place created successfully');
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in ShoppingPlace store.');
+        }
+    }
+
+    /**
+     * Display the shoppingPlace form.
+     */
+    public function edit(Request $request, $id): View
+    {
+        $cities = City::select('id','name')->get();
+        $shoppingPlace  = ShoppingPlace::findOrFail($id);
+        $meta   = [];
+        if(!empty($shoppingPlace->meta_id)){
+            $meta = Meta::findOrFail($shoppingPlace->meta_id);    
+        }
         
-        return view('Shoppingplace.index', compact('shoppingPlaces'));
+        return view('Shoppingplace.edit',compact('cities', 'shoppingPlace', 'meta'));
     }
 
-    public function create()
+    /**
+     * Update the shoppingPlace information.
+     */
+    public function update(Request $request, $id): RedirectResponse
     {
-        $cities = City::all();
-        return view('Shoppingplace.edit', compact('cities'));
+        try {
+            $data = $request->all();
+            $request->merge(['place_id' => $id]);
+            
+            $request->validate([
+                            'place_id'        => 'required|integer|exists:shopping_places,id',
+                            'name'            => 'required|string',
+                            'title'           => 'required|string',
+                            'image'           => 'sometimes|string',
+                            'image_alt'       => 'sometimes|string',
+                            'repeater_content'=> 'sometimes|array',
+                            'city_id'         => 'required|integer|exists:cities,id',
+                            'meta_title'      => 'required|string|nullable',
+                            'meta_description'=> 'required|string|nullable',
+                            'keywords'        => 'required|string|nullable'
+                        ]);
+
+            $shoppingPlace = ShoppingPlace::find($id);
+
+            /* Insert Meta */
+            if((!empty($data['meta_title'])) || (!empty($data['meta_description'])) || (!empty($data['keywords']))){
+                $meta = Meta::where('id',$shoppingPlace->meta_id)
+                            ->update([
+                                'meta_title'       => $data['meta_title'],
+                                'meta_description' => $data['meta_description'],
+                                'keywords'         => $data['keywords'],
+                                'status'           => 1,
+                            ]);
+            }
+            
+            $response = ShoppingPlace::where('id', $shoppingPlace->id)
+                                ->update([
+                                        'name'            => $data['name'],
+                                        'title'           => $data['title'],
+                                        'image'           => $data['image'],
+                                        'image_alt'       => $data['image_alt'],
+                                        'repeater_content'=> $data['repeater_content'],
+                                        'city_id'         => $data['city_id'],
+                                        'meta_id'         => $shoppingPlace->meta_id,
+                                    ]);
+
+
+            return Redirect::route('Shoppingplace.index',$response)->with('success', 'Shopping place updated successfully');
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in ShoppingPlace update.');
+        }
     }
 
-    public function store(Request $request)
+    /**
+     * Delete the shoppingPlace.
+     */
+    public function destroy(Request $request, $id): RedirectResponse
     {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'content' => 'nullable|string',
-            'slug' => 'required|string|max:255',
-            'city_id' => 'required|exists:cities,id',
-            'status'    => 'required|integer|digits_between:0,1',
-        ]);
+        try {
+            $data = $request->all();
+            $request->merge(['place_id' => $id]);
+            $request->validate(['place_id' => 'required|integer|exists:shopping_places,id']);       
 
-        //ShoppingPlace::create($request->all());
+            $shoppingPlace = ShoppingPlace::find($id);
+            if(!empty($shoppingPlace->meta_id)){
+                Meta::destroy($shoppingPlace->meta_id);
+            }
 
-        return redirect()->route('shoppingplace.index')->with('success', 'Shopping place created successfully');
-    }
-
-    public function edit(ShoppingPlace $shoppingPlace)
-    {
-        $cities = City::all();      
-        return view('Shoppingplace.edit', compact('shoppingPlace','cities'));
-    }
-
-    public function update(Request $request, ShoppingPlace $shoppingPlace)
-    {
-        return redirect()->route('shoppingplace.index')->with('success', 'Shopping place updated successfully');
-    }
-
-    public function destroy(ShoppingPlace $shoppingPlace)
-    {
-        $shoppingPlace->delete();
-
-        return redirect()->route('shoppingplace.index')->with('success', 'Shopping place deleted successfully');
+            $response = $shoppingPlace->destroy($id);
+            return Redirect::route('Shoppingplace.index',$response)->with('success', 'Shopping place deleted successfully');
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in ShoppingPlace destroy.');
+        }
     }
 }
