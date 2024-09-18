abcc0e283484d17812b584269b519d19d38fd10c
Kartic Bedi <me@karticbedi.com>
2024-09-18 14:09:38 +0530

    added api for destination

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Destination;
+use App\Models\Meta;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/destination/{slug}', function (Request $request, $slug) {
+    $destination = Destination::where('slug', $slug)->first();
+    if ($destination) {
+        $meta = Meta::find($destination->meta_id);
+        $destination->meta = $meta;
+    }
+    return response()->json($destination);
+});
