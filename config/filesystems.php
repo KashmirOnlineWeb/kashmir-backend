d5a02b4b2beb3926404b7bd019140f9f359b3699
Kartic Bedi <me@karticbedi.com>
2024-09-21 12:24:09 +0530

    path change

--- config/filesystems.php
+++ config/filesystems.php
@@ -32,7 +32,7 @@ return [
 
         'local' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('app/public/storage'),
             //'root' => public_path('/'),
             'throw' => false,
         ],
