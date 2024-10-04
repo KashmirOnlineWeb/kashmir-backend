b28e059a0412fe79893ae339280d5534fe944918
Chander Bedi <chanderbedi88@gmail.com>
2024-10-04 18:51:06 +0530

    Create: categories methods.

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -65,24 +65,64 @@ class CategoryController extends Controller
     /**
      * Display the category form.
      */
-    public function edit(Request $request): View
+    public function edit(Request $request, $id): View
     {
-        return view('category.edit');
+        $category  = Category::findOrFail($id);
+        return view('Category.edit',compact('category'));
     }
 
     /**
      * Update the category information.
      */
-    public function update(): RedirectResponse
+    public function update(Request $request, $id): RedirectResponse
     {
-        
+        try {
+            $data = $request->all();
+            $request->merge(['category_id' => $id]);
+            
+            $request->validate([
+                            'category_id' => 'required|integer|exists:categories,id',
+                            'name'        => 'required|string',
+                            'status'      => 'required|integer|digits_between:0,1',
+                            'slug'        => 'required|string',
+                            'min_price'   => 'numeric',
+                        ]);
+
+            $category = Category::find($id);
+            
+            $response = Category::where('id', $category->id)
+                                ->update([
+                                        'name'      => $data['name'],
+                                        'image'     => $data['image'],
+                                        'image_alt' => $data['image_alt'],
+                                        'status'    => $data['status'],
+                                        'slug'      => $data['slug'],
+                                        'min_price' => $data['min_price']
+                                    ]);
+
+
+            return Redirect::route('category.index',$response);
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in category update.');
+        }
     }
 
     /**
      * Delete the category.
      */
-    public function destroy(Request $request): RedirectResponse
+    public function destroy(Request $request, $id): RedirectResponse
     {
-        
+        try {
+            $data = $request->all();
+            $request->merge(['category_id' => $id]);
+                
+            $request->validate(['category_id' => 'required|integer|exists:categories,id']);
+            $category = Category::find($id);
+            
+            $response = $category->destroy($id);
+            return Redirect::route('category.index',$response);
+        } catch (Exception $e) {
+            Log::error('Somethinng went wrong in category destroy.');
+        }
     }
 }
