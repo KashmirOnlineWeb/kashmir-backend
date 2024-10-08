bf36da7d0aac0f08b5378f00de18f82c89df7ffe
Kartic Bedi <me@karticbedi.com>
2024-10-08 16:46:00 +0530

    changed meta from string to text

--- database/migrations/2024_09_09_102051_create_metas_table.php
+++ database/migrations/2024_09_09_102051_create_metas_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('metas', function (Blueprint $table) {
             $table->id();
-            $table->string('meta_title')->nullable();
-            $table->string('meta_description')->nullable();
-            $table->string('keywords')->nullable();
+            $table->text('meta_title')->nullable();
+            $table->text('meta_description')->nullable();
+            $table->text('keywords')->nullable();
             $table->tinyInteger('status')->default(0)->comment('0=inactive,1=active');
             $table->timestamps();
         });
